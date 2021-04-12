fd6281c15a2e554d75aa14e845b60b5e3be84b59
Tobechi <38112880+michaelnnakwe@users.noreply.github.com>
2021-04-12 12:00:22 +0100

    first commit

---
+++ src/main/java/com/tobechi/App.java
@@ -0,0 +1,13 @@
+package com.tobechi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
